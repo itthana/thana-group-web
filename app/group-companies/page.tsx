f51005219a6638e0f7699f1d876043dccaac1d2f
Business Development <bd@MacBookNeobd.local>
2026-07-09 13:36:06 +0700

    Update subsidiary company logos

--- app/group-companies/page.tsx
+++ app/group-companies/page.tsx
@@ -29,7 +29,7 @@ const groupCompaniesData = [
     name: 'บริษัท ธนาโลจิสติกส์ จำกัด',
     abbr: '',
     description: 'ให้บริการขนส่งระหว่างประเทศ (Cross-border Transport) ครอบคลุมไทย-ลาว-อาเซียน ด้วยกองทัพรถบรรทุกมาตรฐานสากล',
-    logo: 'LOGO-TLT.png',
+    logo: '0000.png',
     contact: {
       tel: '045-222-222',
       line: '@thanagroup',
